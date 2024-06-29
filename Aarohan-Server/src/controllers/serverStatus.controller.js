63148a926529d8a6a94b481a50cdd09e5cffe0db
Rohan Dey <deyrohan16305@gmail.com>
2024-06-29 10:15:12 +0530

    update

--- Aarohan-Server/src/controllers/serverStatus.controller.js
+++
@@ -1,9 +0,0 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-
-const serverStatus = asyncHandler(async(req, res) => {
-    return res
-    .status(200)
-    .send("<h1>⚙️ Server is running...</h1>")
-})
-
-export {serverStatus}
--- Aarohan-Server/src/routes/serverStatus.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { serverStatus } from "../controllers/serverStatus.controller.js";
-
-const router = Router()
-
-router.route("/").get(serverStatus)
-
-export default router
