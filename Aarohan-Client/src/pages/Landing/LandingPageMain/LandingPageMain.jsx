675a688fd15d5769f72ea2b917205de2c850c149
Anushka Banerjee <anushkabanerjee03@gmail.com>
2024-06-29 11:45:40 +0530

    Update landing page UI for mobile menu and sign in button

--- Aarohan-Client/src/pages/Landing/LandingPageMain/LandingPageMain.jsx
+++ Aarohan-Client/src/pages/Landing/LandingPageMain/LandingPageMain.jsx
@@ -28,27 +28,20 @@ export default function LandingPageMain() {
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              {mobileMenuOpen===false && <Bars3Icon className="h-6 w-6" aria-hidden="true" />}
             </button>
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link to="/login" className="text-base font-semibold leading-6 text-white-default">
-              Log in <span aria-hidden="true">&rarr;</span>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end bg-yellow-dark max-w-fit py-2 px-4 rounded-md">
+            <Link to="/login" className="text-base font-semibold leading-6 text-blue-dark">
+              Sign In
             </Link>
           </div>
         </nav>
         <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10  backdrop-blur-xl h-1/3">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </a>
+              
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -61,12 +54,12 @@ export default function LandingPageMain() {
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="py-6">
-                  <a
-                    href="#"
+                  <Link
+                    to="/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
-                    Log in
-                  </a>
+                    Sign in
+                  </Link>
                 </div>
               </div>
             </div>
